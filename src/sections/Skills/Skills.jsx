d0eccf9c9f53b6d5686b13c2bd6fefc2cab4925f
Akshita <akshita2000singh@gmail.com>
2024-10-20 15:27:58 -0400

    Removed skills from mobile view

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -17,13 +17,14 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="R" />
         <SkillList src={checkMarkIcon} skill="C/C++" />
         <SkillList src={checkMarkIcon} skill="SQL" />
-        <SkillList src={checkMarkIcon} skill="Web (HTML, CSS, JS, React)" />
+        <SkillList src={checkMarkIcon} skill="Cipher" />
+        <SkillList src={checkMarkIcon} skill="Web (HTML, CSS, JavaScript, React)" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Pandas" />
         <SkillList src={checkMarkIcon} skill="Numpy" />
-        <SkillList src={checkMarkIcon} skill=" Scikit-Learn" />
+        <SkillList src={checkMarkIcon} skill="Scikit-Learn" />
         <SkillList src={checkMarkIcon} skill="TensorFlow" />
         <SkillList src={checkMarkIcon} skill="OpenCV" />
         <SkillList src={checkMarkIcon} skill="NLTK" />
@@ -32,15 +33,16 @@ function Skills() {
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill=" RDBMS (MySQL, PostgreSQL)" />
+        <SkillList src={checkMarkIcon} skill="RDBMS (MySQL, PostgreSQL)" />
         <SkillList src={checkMarkIcon} skill="Neo4j" />
         <SkillList src={checkMarkIcon} skill="ETL/ELT" />
         <SkillList src={checkMarkIcon} skill="Data Modeling" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill=" Cloud (AWS, GCP)" />
+        <SkillList src={checkMarkIcon} skill="Cloud (AWS, GCP)" />
         <SkillList src={checkMarkIcon} skill="Git" />
+        <SkillList src={checkMarkIcon} skill="Docker" />
         <SkillList src={checkMarkIcon} skill="Airflow" />
         <SkillList src={checkMarkIcon} skill="Mlflow" />
         <SkillList src={checkMarkIcon} skill="LLMs (GenAI)" />
